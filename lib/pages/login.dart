31cb6c9794cb956f7a25aa22adf6804d269ac2af
Jarinya008 <jarinyawhan@gmail.com>
2025-01-12 16:44:33 +0700

    lp

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,6 +10,6 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(appBar: AppBar(), body: SingleChildScrollView());
   }
 }
